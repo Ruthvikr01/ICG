926dc83478f9371d9a3e7bf2e1b37a269db8b3f7
ARDHIKIRAN <75793270+ARDHIKIRAN@users.noreply.github.com>
2023-06-19 19:45:50 +0530

    Update package.py

--- package.py
+++ package.py
@@ -1,5 +1,10 @@
+# installing and import all the dependicies
+# command to be executed in terminal
+# pip3 install tensorflow -> installing tensorflow and keras
+# pip3 install opencv-python -> installing opencv 
 import tensorflow as tf
 from tensorflow import keras
 import cv2
+# checking the version of tensorflow and keras
 print("TensorFlow version:", tf.__version__)
 print("OpenCV version:", cv2.__version__)
